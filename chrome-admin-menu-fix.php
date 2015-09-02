6f4f9b4d92493bfca6d0ac5833fb42862cfecfc9
Joe Fletcher <Joe@merchantguru.com>
2015-09-02 09:42:32 -0700

    Only apply fix to Chrome browsers
    So Firefox et al can continue without the collapsing

--- chrome-admin-menu-fix.php
+++ chrome-admin-menu-fix.php
@@ -4,21 +4,30 @@ Plugin Name: Chrome Admin Menu Fix
 Description: Quick and dirty fix for the Chrome menu display glitches with Open Sans / WordPress
 Author: Steve Jones for The Space Between
 Author URI: http://the--space--between.com
-Version: 1.0.0
+Version: 1.0.1
 */
 
 namespace TheSpaceBetween\Tools;
 
 function load_scripts()
 {
-	// Need jQuery
-	wp_enqueue_script('jquery');
+	$ua = $_SERVER['HTTP_USER_AGENT'];
 
-	// Path to our simple script
-	$js_path = plugins_url( 'js/chrome-menu-admin-fix.js', __FILE__ );
+	if (strpos($ua,'Chrome/') > -1)
 
-	// Load the simple script to click the collapse button twice
-	wp_enqueue_script( 'chrome-menu-admin-fix', $js_path, array('jquery'), '1.0.0', true );
+	{
+		// Chrome found!
+
+		// Need jQuery
+		wp_enqueue_script('jquery');
+
+		// Path to our simple script
+		$js_path = plugins_url( 'js/chrome-menu-admin-fix.js', __FILE__ );
+
+		// Load the simple script to click the collapse button twice
+		wp_enqueue_script( 'chrome-menu-admin-fix', $js_path, array('jquery'), '1.0.0', true );
+	}
+
 }
 
 add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\load_scripts' );
