8710e9babbec4914f8f47e34f95d70d3063f283c
Steve Jones <steve@thespacebetween.london>
2015-09-02 21:33:03 +0100

    Inline javascript suggested  by @Otto42

--- chrome-admin-menu-fix.php
+++ chrome-admin-menu-fix.php
@@ -7,27 +7,32 @@ Author URI: http://the--space--between.com
 Version: 1.0.1
 */
 
-namespace TheSpaceBetween\Tools;
-
-function load_scripts()
+function chromefix_admin_init()
 {
-	// Check to see if Chrome is being used
-	$ua = $_SERVER['HTTP_USER_AGENT'];
-
-	if (strpos($ua,'Chrome/') > -1)
+	if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Chrome' ) !== false ) 
 	{
-		// Chrome found!
-
-		// Need jQuery
-		wp_enqueue_script('jquery');
-
-		// Path to our simple script
-		$js_path = plugins_url( 'js/chrome-menu-admin-fix.js', __FILE__ );
-
-		// Load the simple script to click the collapse button twice
-		wp_enqueue_script( 'chrome-menu-admin-fix', $js_path, array('jquery'), '1.0.0', true );
+		add_action( 'admin_enqueue_scripts', 'chromefix_enqueue_scripts' );
+		add_action( 'admin_footer', 'chromefix_print_js' );
 	}
-
 }
 
-add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\load_scripts' );
+function chromefix_enqueue_scripts()
+{
+	wp_enqueue_script('jquery');
+}
+
+// Inline script inspired by Samuel Wood - http://ottopress.com
+function chromefix_print_js()
+{ 
+?>
+	<script type='text/javascript'>
+		(function($){
+			$(window).load(function(){
+				$('#adminmenuwrap').hide().show();
+			});
+		})(jQuery);
+	</script>
+<?
+}
+
+add_action( 'admin_init', 'chromefix_admin_init' );
