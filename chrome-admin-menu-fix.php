bd01cfee251a9a4cafd71253e50bed3a7350be0f
Steve Jones <steve@thespacebetween.london>
2015-09-03 21:45:04 +0100

    Removed Chrome browser check

--- chrome-admin-menu-fix.php
+++ chrome-admin-menu-fix.php
@@ -4,15 +4,12 @@ Plugin Name: Chrome Admin Menu Fix
 Description: Quick fix for the Chrome 45 admin menu display glitches
 Author: Steve Jones for The Space Between / Samuel Wood / Otto42
 Author URI: http://the--space--between.com
-Version: 2.0.0
+Version: 2.1.0
 */
 
 function chromefix_inline_css()
 { 
-	if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Chrome' ) !== false )
-	{
-		wp_add_inline_style( 'wp-admin', '#adminmenu { transform: translateZ(0); }' );
-	}
+	wp_add_inline_style( 'wp-admin', '#adminmenu { transform: translateZ(0); }' );
 }
 
 add_action('admin_enqueue_scripts', 'chromefix_inline_css');
