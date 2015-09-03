d09b9e2d12c1606085b0a3630165b6945b6e5662
Steve Jones <steve@thespacebetween.london>
2015-09-03 12:54:04 +0100

    Simpler fix via @Otto42

--- chrome-admin-menu-fix.php
+++ chrome-admin-menu-fix.php
@@ -2,31 +2,19 @@
 /*
 Plugin Name: Chrome Admin Menu Fix
 Description: Quick fix for the Chrome 45 admin menu display glitches
-Author: Steve Jones for The Space Between / Samuel Wood
+Author: Steve Jones for The Space Between / Samuel Wood / Otto42
 Author URI: http://the--space--between.com
 Version: 1.2.0
 */
 
-function chromefix_admin_init()
-{
-	if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Chrome' ) !== false ) 
+function chromefix_inline_css()
+{ 
+	if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Chrome' ) !== false )
 	{
-		add_action( 'admin_print_styles', 'chromefix_print_css' );
+		wp_add_inline_style( 'wp-admin', '#adminmenu { transform: translateZ(0); }' );
 	}
 }
 
-// Inline script inspired by Samuel Wood - http://ottopress.com
-function chromefix_print_css()
-{ 
-?>
-	<style type="text/css" media="screen">
-		#adminmenu { 
-			transform: translateZ(0);
-		}
-	</style>
-<?php
-}
-
-add_action( 'admin_init', 'chromefix_admin_init' );
+add_action('admin_enqueue_scripts', 'chromefix_inline_css');
 
 ?>
